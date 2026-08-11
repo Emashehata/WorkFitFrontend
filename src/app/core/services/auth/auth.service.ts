7ee2c6b0acd53f9d075d4c5ba2ebf99cf440a890
Eman Shehata <emanshehata258@gmail.com>
2026-08-11 22:57:46 +0300

    update auth

--- src/app/core/services/auth/auth.service.ts
+++ src/app/core/services/auth/auth.service.ts
@@ -32,7 +32,7 @@ export class AuthService {
 
   // Primary Role
   readonly role = computed<UserRole | null>(() => {
-    return this.getUserRoles()[1] ?? null;
+    return this.getUserRoles()[0] ?? null;
   });
 
   // Role Signals
